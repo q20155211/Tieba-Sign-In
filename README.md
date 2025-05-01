63ec78bf3feb9b084741c2fdd14802a5e4abc081
q20155211 <118663649+q20155211@users.noreply.github.com>
2025-05-01 19:50:20 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Tieba-Sign-In
 基于Python的aiotieba库并使用Github Action实现贴吧每日自动签到
+
+### 今日签到状态
+
+![Baidu Tieba Auto Sign](https://github.com/gwtak/TieBaSign/workflows/Baidu%20Tieba%20Auto%20Sign/badge.svg)
+
+## 使用说明
+
+Fork 本仓库，然后点击你的仓库右上角的 Settings，找到 Secrets 这一项，添加一个库秘密变量。其中 `BDUSS` 存放你的 BDUSS。支持同时添加多个帐户，BDUSS 之间用 `#` 隔开即可。
+### 如何获取BDUSS：
+  在网页中登录上贴吧，然后按下`F12`打开调试模式，在`cookie`中找到`BDUSS`，并复制其`Value`值。
+or登录tieba lite，在 设置 - 账号管理 中复制BDUSS
+
+#### 注意
+使用BDUSS可以完成一切不需要手机/邮箱验证码的操作，包括发帖/发私信/获取账号上的所有历史发言
+BDUSS的过期时间长达数年，一般只能通过退出登录或修改密码使其失效
+因此将BDUSS泄露给不受信任的人可能导致长期的账号安全风险和隐私泄露风险
---
+++ test tieba.py
@@ -0,0 +1,73 @@
+import aiotieba as tb
+import asyncio
+import time
+import os
+import sys
+
+BDUSS = os.getenv("BDUSS")
+if not BDUSS:
+    print("未设置环境变量 BDUSS , 程序退出")
+    sys.exit()
+BDUSS_SPLIT=BDUSS.split("#")
+user_count=1
+
+async def main(BDUSS):
+    async with tb.Client(BDUSS=BDUSS) as client:
+        
+        user = await client.get_self_info()
+        print(f"用户名: {user.user_name}")
+        print(f"id：{user.user_id}")
+        print(f"uid: {user.tieba_uid}")
+        print(f"吧龄: {user.age} 年")
+        print(f"昵称: {user.nick_name_new}")
+        print(f"portrait: {user.portrait}")
+        print("\n")
+
+        print("获取关注贴吧列表并签到...")
+        print("\n")
+        followed_forums_pages=1
+        followed_forums_count=0
+        sign_in_success_count=0
+        sign_in_already_count=0
+        sign_in_failed_count=0
+        while True:
+            followed_forums=await client.get_follow_forums(id_=user.user_id,pn=followed_forums_pages)
+            for x in followed_forums.objs:
+                print(f"{x.fname} ({x.fid})",end=" ")
+                
+                sign_in_result=await client.sign_forum(x.fname)
+                
+                if not sign_in_result.err:
+                    sign_in_success_count+=1
+                    print(f"签到成功")
+                elif sign_in_result.err.args[0]==160002:
+                    sign_in_already_count+=1
+                    print(f"已签到")
+                else:
+                    sign_in_failed_count+=1
+                    print(f"签到失败，错误：{sign_in_result.err}")
+                print("")
+
+                followed_forums_count+=1
+                time.sleep(5)
+            followed_forums_pages+=1
+            if not followed_forums.has_more:
+                break
+
+        print(f"总关注贴吧数: {followed_forums_count} 个")
+        print(f"已签到: {sign_in_already_count} 个")
+        print(f"签到成功: {sign_in_success_count} 个")
+        print(f"签到失败: {sign_in_failed_count} 个")
+        print(f"签到率: {(sign_in_success_count+sign_in_already_count)*100/followed_forums_count}%")
+
+for i in BDUSS_SPLIT:
+    print(f"使用第 {user_count} 个 BDUSS 进行签到...")
+    print(f"获取第 {user_count} 个用户信息...")
+    asyncio.run(main(i))
+    if user_count<len(BDUSS_SPLIT):
+        print(f"第 {user_count} 个用户签到完成，等待 60 秒...")
+        time.sleep(60)
+        print("\n\n\n")
+    user_count+=1
+
+print("所有用户签到完成")
